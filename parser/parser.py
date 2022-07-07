ec80f8432359ca8defb13066e49c3c10b7e81c21
AyahBot Dev <96742838+AyahBot-Dev@users.noreply.github.com>
2022-07-07 19:52:17 +0600

    Update parser.py

--- parser/parser.py
+++ parser/parser.py
@@ -19,14 +19,18 @@ except:
     pass
 
 for i in tqdm(range(114), desc="ar"):
-  a_r = requests.get(ar_url.format(str(i+1)))
-  a_v = a_r.json()["verses"]
-  psv = ""
-  for j in range(len(a_v)):
-    psv += "{}\n".format(a_v[j]["text_uthmani"])
-  f = open("../quran/ar/{}.psv".format(str(i+1)), "w", encoding="utf-8")
-  f.write(psv)
-  f.close()
+    try:
+      a_r = requests.get(ar_url.format(str(i+1)))
+      a_v = a_r.json()["verses"]
+      psv = ""
+      for j in range(len(a_v)):
+        psv += "{}\n".format(a_v[j]["text_uthmani"])
+      f = open("../quran/ar/{}.psv".format(str(i+1)), "w", encoding="utf-8")
+      f.write(psv)
+      f.close()
+    except:
+        print("faced an error in ar surah {}".format(str(i+1)))
+        continue
 
 for k in range(len(tr_arr)):
   try:
@@ -34,14 +38,18 @@ for k in range(len(tr_arr)):
   except:
       print("\'{}\' directory already exists".format(str(tr_arr[k]["id"])))
   for i in tqdm(range(114), desc=("%d index" % k)):
-    t_r = requests.get(tr_url.format(str(i+1), str(tr_arr[k]["id"])))
-    t_v = t_r.json()["verses"]
-    psv = ""
-    for j in range(len(t_v)):
-      psv += "{}\n".format(re.sub(re.compile('\s\s+'), " ", re.sub(re.compile("<[^<]+?>\d*"), " ", t_v[j]["translations"][0]["text"]).replace("\r\n", "").replace("\n", "").replace("\xa0","")))
-    f = open("../quran/{}/{}.psv".format(str(tr_arr[k]["id"]), str(i+1)), "w", encoding="utf-8")
-    f.write(psv)
-    f.close()
+    try:
+        t_r = requests.get(tr_url.format(str(i+1), str(tr_arr[k]["id"])))
+        t_v = t_r.json()["verses"]
+        psv = ""
+        for j in range(len(t_v)):
+          psv += "{}\n".format(re.sub(re.compile('\s\s+'), " ", re.sub(re.compile("<[^<]+?>\d*"), " ", t_v[j]["translations"][0]["text"]).replace("\r\n", "").replace("\n", "").replace("\xa0","")))
+        f = open("../quran/{}/{}.psv".format(str(tr_arr[k]["id"]), str(i+1)), "w", encoding="utf-8")
+        f.write(psv)
+        f.close()
+    except:
+        print("faced an error while in {} index and {} trans and surah {}".format(k, str(tr_arr[k]["id"]), str(i+1)))
+        continue
   sleep(5)
 
 
